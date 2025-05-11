dbd6c64d6aad0cb6fcfaa7269c2cf4015b902754
Abduholiq <uzb98769@gmail.com>
2025-05-11 12:25:32 -0800

    Delete Desktop directory
    Signed-off-by: Abduholiq <uzb98769@gmail.com>

--- Desktop/New folder/script.js
+++
@@ -1,5 +0,0 @@
-function changeStyle(styleName) {
-    const gallery = document.querySelector('.gallery');
-    gallery.classList.remove('style1','style2','style3');
-    gallery.classList.add(styleName);
-}
